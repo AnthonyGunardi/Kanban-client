809dd2d9add9394c261eb8eab51489255f51b0d5
AnthonyGunardi <anthonygunardi@gmail.com>
2021-01-14 19:30:47 +0700

    initiate vue component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+
+<script>
+import axios from 'axios'
+import Vue from 'vue'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            pageName: 'landingpage',
+            tasks: [],
+            loginState: 'login'
+        };
+    },
+    methods: {
+
+    },
+    components: {
+      
+    }
+}
+</script>
+
+<style>
+
+</style>
