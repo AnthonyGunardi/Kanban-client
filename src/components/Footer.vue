260f5ce9745a7aff03d0947be06dd9f81c7504ca
AnthonyGunardi <anthonygunardi@gmail.com>
2021-01-14 22:14:57 +0700

    create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+        <div class="footer-copyright text-center py-3 text-white">Copyright © 2021 Anthony Gunardi. All Rights Reserved | Author : <a href="https://github.com/AnthonyGunardi" target="_blank">Anthony Gunardi</a> | <a href="https://github.com/AnthonyGunardi/kanban-client" target="_blank">Help</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
